9ca842e9b9bdec069a3aa8419d0456a8294bb4e9
lorena252 <lorenaavalos252@hotmail.com>
2024-01-26 14:21:36 -0300

    i modify header styles and add icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import {useState , useEffect} from "react"
-import { Button, Box} from "@chakra-ui/react";
+import { Button, Box,Icon} from "@chakra-ui/react";
 import {  useNavigate } from "react-router-dom" 
-export default function Header() {
+  import { PiPopcornBold } from "react-icons/pi";
 
+
+export default function Header(){
 const navigate = useNavigate()
 
   return (
-    <Box h='100%' bg="black" >
-      <Button onClick={() => navigate("/")} m="2" colorScheme='cyan' variant="ghost" >
+    <Box h='100px' bg="black" >
+          <Icon boxSize={14} color="pink" pt="2" ml="2" as={PiPopcornBold} />        
+      <Button onClick={() => navigate("/")} m="2" h="90px" w="150px" size='lg' colorScheme='pink' variant='ghost'>
         Home
       </Button>
-      <Button onClick={() => navigate("/lanzamientos")} m="2"  colorScheme='cyan' variant="ghost">
+      <Button onClick={() => navigate("/lanzamientos")} m="2" size='lg'  h="90px" colorScheme='pink' variant='ghost'>
         Ultimos lanzamientos
       </Button>
-      <Button onClick={() => navigate("/populares")} m="2" colorScheme='cyan' variant="ghost">
+      <Button onClick={() => navigate("/populares")} m="2" w="150px"   size='lg' h="90px" colorScheme='pink' variant='ghost'>
         Populares
       </Button>
-      <Button onClick={() => navigate("/buscar")} m="2" colorScheme='cyan' variant="ghost">
+      <Button onClick={() => navigate("/buscar")} m="2" w="150px"  size='lg' h="90px" colorScheme='pink' variant='ghost'  >
         Buscar
       </Button>
     </Box>
