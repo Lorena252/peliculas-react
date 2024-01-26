63ab128879ef2bf7a5b05f930c08c01330f66f4e
lorena252 <lorenaavalos252@hotmail.com>
2024-01-26 14:34:45 -0300

    create component HomeList

---
+++ src/components/HomeList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Text,Box,Icon} from "@chakra-ui/react";
+import {  useNavigate } from "react-router-dom" 
+
+
+
+export default function HomeLists(){
+return(
+<>
+
+</>
+)
+}
