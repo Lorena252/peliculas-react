ebe4abdee6959e8c80a6a3ad9226db80a09ac5dd
lorena252 <lorenaavalos252@hotmail.com>
2024-01-31 01:38:01 -0300

    added card and star icons

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -11,21 +11,32 @@ import {
   Divider,
   CardFooter,
   Box,
-
+Icon
 } from "@chakra-ui/react";
-import {ViewIcon } from '@chakra-ui/icons'
+
+import {ViewIcon} from '@chakra-ui/icons'
+import {FavoritesContext} from "../context/FavoritesContext"
+import { useContext } from "react";
+import { FaRegStar, FaStar } from "react-icons/fa6";
+
 
 
 export default function CardMovie({id,title,img}) {
 
-const navigate = useNavigate()
+ const {addFavs, delFavs,isFavs} = useContext(FavoritesContext)
+
+ const navigate = useNavigate()
 
   return (
-    <Box onClick={() => navigate(`/detail/${id}`)}  >
+  <>
+
+<Box>
+
+    <Box onClick={() => navigate(`/detail/${id}`)}>
       <Box  w="100%">
         <Flex>
           <Wrap>
-            <Card w="280px" m="2px" mb="25px"  >
+            <Card w="280px" m="2px" br="red" >
               <Box
               h="500px"
               bgSize="cover"
@@ -49,14 +60,29 @@ const navigate = useNavigate()
                 <CardFooter>
                   <ViewIcon />
                 </CardFooter>
+               
               </Center>
             </Card>
           </Wrap>
         </Flex>
       </Box>
-
-      
     </Box>
-   
+
+
+ <Box mb="15px" >
+  <Card  h="35px">
+  <Center>
+     <Icon mt="10px" as={FaStar} color="yelow"  onClick={addFavs}/>
+  <Icon mt="10px" as={FaRegStar} onClick={delFavs}  />
+ 
+  </Center>
+    </Card>
+  </Box>
+
+  
+
+</Box>
+
+   </>
   );
 }
